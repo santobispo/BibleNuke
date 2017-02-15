b5c7797a6b129c51e040391a86f6cd3f69d169bc
santobispo <santobispo@gmail.com>
2017-02-14 21:35:59 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+/* ***
+ *  So quero ver isso
+ * /
+ */
+ 
 var json_sel_book = [
 	{"index": false, "name": false, "chapters": false, "chapter": false, "verses": false, "verse": false}
 	];
@@ -21,7 +26,8 @@ var json_language = [
 	];
 
 var sumary = [
-	[0,"G&ecirc;nesis",50],[1,"&ecirc;xodo",40],[2,"Lev&iacute;tico",27],[3,"N&uacute;meros",36],[4,"Deuteron&ocirc;mio",34],[5,"Josu&eacute;",24],[6,"Ju&iacute;zes",21],[7,"Rute",4],[8,"1 Samuel",31],[9,"2 Samuel",24],[10,"1 Reis",22],[11,"2 Reis",25],[12,"1 Cr&ocirc;nicas",29],[13,"2 Cr&ocirc;nicas",36],[14,"Esdras",10],[15,"Neemias",13],[16,"Ester",10],[17,"J&oacute;",42],[18,"Salmos",150],[19,"Prov&eacute;rbios",31],[20,"Eclesiastes",12],[21,"Cânticos",8],[22,"Isa&iacute;as",66],[23,"Jeremias",52],[24,"Lamentaç&otilde;es de Jeremias",5],[25,"Ezequiel",48],[26,"Daniel",12],[27,"Os&eacute;ias",14],[28,"Joel",3],[29,"Am&oacute;s",9],[30,"Obadias",1],[31,"Jonas",4],[32,"Miqu&eacute;ias",7],[33,"Naum",3],[34,"Habacuque",3],[35,"Sofonias",3],[36,"Ageu",2],[37,"Zacarias",14],[38,"Malaquias",4],[39,"Mateus",28],[40,"Marcos",16],[41,"Lucas",24],[42,"Jo&atilde;o",21],[43,"Atos",28],[44,"Romanos",16],[45,"1 Cor&iacute;ntios",16],[46,"2 Cor&iacute;ntios",13],[47,"Gálatas",6],[48,"Ef&eacute;sios",6],[49,"Filipenses",4],[50,"Colossenses",4],[51,"1 Tessalonicenses",5],[52,"2 Tessalonicenses",3],[53,"1 Tim&oacute;teo",6],[54,"2 Tim&oacute;teo",4],[55,"Tito",3],[56,"Filemom",1],[57,"Hebreus",13],[58,"Tiago",5],[59,"1 Pedro",5],[60,"2 Pedro",3],[61,"1 Jo&atilde;o",3],[62,"2 Jo&atilde;o",1],[63,"3 Jo&atilde;o",1],[64,"Judas",1],[65,"Apocalipse",22]];
+	[0,"G&ecirc;nesis",50],[1,"&ecirc;x
+	odo",40],[2,"Lev&iacute;tico",27],[3,"N&uacute;meros",36],[4,"Deuteron&ocirc;mio",34],[5,"Josu&eacute;",24],[6,"Ju&iacute;zes",21],[7,"Rute",4],[8,"1 Samuel",31],[9,"2 Samuel",24],[10,"1 Reis",22],[11,"2 Reis",25],[12,"1 Cr&ocirc;nicas",29],[13,"2 Cr&ocirc;nicas",36],[14,"Esdras",10],[15,"Neemias",13],[16,"Ester",10],[17,"J&oacute;",42],[18,"Salmos",150],[19,"Prov&eacute;rbios",31],[20,"Eclesiastes",12],[21,"Cânticos",8],[22,"Isa&iacute;as",66],[23,"Jeremias",52],[24,"Lamentaç&otilde;es de Jeremias",5],[25,"Ezequiel",48],[26,"Daniel",12],[27,"Os&eacute;ias",14],[28,"Joel",3],[29,"Am&oacute;s",9],[30,"Obadias",1],[31,"Jonas",4],[32,"Miqu&eacute;ias",7],[33,"Naum",3],[34,"Habacuque",3],[35,"Sofonias",3],[36,"Ageu",2],[37,"Zacarias",14],[38,"Malaquias",4],[39,"Mateus",28],[40,"Marcos",16],[41,"Lucas",24],[42,"Jo&atilde;o",21],[43,"Atos",28],[44,"Romanos",16],[45,"1 Cor&iacute;ntios",16],[46,"2 Cor&iacute;ntios",13],[47,"Gálatas",6],[48,"Ef&eacute;sios",6],[49,"Filipenses",4],[50,"Colossenses",4],[51,"1 Tessalonicenses",5],[52,"2 Tessalonicenses",3],[53,"1 Tim&oacute;teo",6],[54,"2 Tim&oacute;teo",4],[55,"Tito",3],[56,"Filemom",1],[57,"Hebreus",13],[58,"Tiago",5],[59,"1 Pedro",5],[60,"2 Pedro",3],[61,"1 Jo&atilde;o",3],[62,"2 Jo&atilde;o",1],[63,"3 Jo&atilde;o",1],[64,"Judas",1],[65,"Apocalipse",22]];
 
 var json_marked = [];
 
@@ -139,6 +145,9 @@ var shelf=false;
 			closeNav();
 		}
 		
+		/* Vamos pagar pra ver
+		 */
+		  
 		function getBibleAux (lang) {
 			var url = "json/"+lang+".json"; 
 			var jqxhr = $.getJSON(url, function(data) {
